d3a6afe789194f6a493a1adc571ac3fe1586a50e
MuhdAminRahman <lonemin1@gmail.com>
2025-07-18 17:14:48 +0800

    added: OrderItem related modules

---
+++ myproject/src/main/java/com/fooddeliveryplatform/httphandler/OrderItemApiHandler.java
@@ -0,0 +1,18 @@
+package com.fooddeliveryplatform.httphandler;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class OrderItemApiHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        // Implementation for handling HTTP requests related to OrderItems
+        // This could include creating, retrieving, updating, or deleting order items
+    }
+
+    // Additional methods for handling specific routes can be added here
+    
+}
